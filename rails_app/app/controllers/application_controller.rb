1e11cd4a3d039cb5a521583b1632ddb73286f788
Inai <37035249+inaishunsuke@users.noreply.github.com>
2018-12-27 13:56:38 +0900

    API化の試行錯誤
    usersテーブルの削除。
    --apiオプションで作り直しするため、こちらは使用しないこととする。

--- rails_app/app/controllers/application_controller.rb
+++ rails_app/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+#class ApplicationController < ActionController::API
+#  protect_from_forgery with: :null_session
 end
--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_143645) do
+ActiveRecord::Schema.define(version: 2018_12_20_112356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "mail"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
